1995f1375f61b38d9a63a915b64b51288beb0347
owais3351 <owaissofi3351@gmail.com>
2025-03-17 18:18:31 +0530

    Logging And Exception

--- src/exception.py
+++ src/exception.py
@@ -1,25 +1,21 @@
-# exception.py module inside src package is used for exception handling
-# code Implementation
 import sys
 
-# Define Exception Funcation--> store detailed info about the exception
+# Exception Fincation:
+
 def error_message_detail(error,error_detail : sys):
-     _,_,exc_tb=error_detail.exc_info()
-     file_name=exc_tb.tb_frame.f_code.co_filename
-     error_message="Error occured in python script name[{0}] line number[{1}] error message[{2}]".format(file_name,exc_tb.tb_lineno,str(error))
+    exc_type,exc_value,exc_tb=error_detail.exc_info()
+    filename=exc_tb.tb_frame.f_code.co_filename
+    error_message="Error occured in python name [{0}] line number[{1}] error message[{2}]".format(filename,exc_tb.tb_lineno,str(error))
 
-     return error_message
-
-# Define custom exception
-# we can create custom exception by defining a new class
-# Useful when we want to handle specific errors  in our application in more descriptive way
+    return error_message
 
 class CustomException(Exception):
-     def __init__(self,error_message,error_detail : sys ):
-          super().__init__(error_message) 
-          self.error_message=error_message_detail(error_message,error_detail=error_detail)
+    def __init__(self,error_message,error_detail :sys):
+        super().__init__(error_message)
+        self.error_message=error_message_detail(error_message,error_detail=error_detail)
 
-     def __str__(self):
-      return self.error_message
-     
-
+    def __str__(self):
+        return self.error_message
+    
+if __name__=="__main__":
+    print("exception handling")
--- src/logger.py
+++ src/logger.py
@@ -2,21 +2,23 @@ import logging
 import os
 from datetime import datetime
 
-#create log-file based on current date and time:
+# create LOG file with current date and time
 LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 
-#Full file path for Storing log-file inside Logs directory within CWD:
-logs_path=os.path.join(os.getcwd(),"Logs",LOG_FILE)
+#Full file path for storing logfile inside directory-->{logs} within CWD:
+logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
 
-#create directory(Logs) if it do not exist:
-os.makedirs(logs_path,LOG_FILE) 
+#create a directory(logs) if it doesn't exist
+os.makedirs(logs_path,exist_ok=True)
 
-LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
+LOG_FILE_path=os.path.join(logs_path,LOG_FILE)
 
-
-#Basic logging configuration:
+#basic logging Configuration
 logging.basicConfig(
-    filename=LOG_FILE_PATH,
-    format="[%(asctime)s] %(lineno)d %(name)s-%(levelname)s-%(message)s",
-    level=logging.INFO,
+    filename=LOG_FILE_path,
+    format="[%(asctime)s] %(lineno)s %(name)s-%(levelname)s-%(message)s",
+    level=logging.INFO
 )
+
+if __name__=="__main__":
+    print("logging")
