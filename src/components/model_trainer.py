85ac348929e9ad55a984d16e06143cbb7fe69f44
owais3351 <owaissofi3351@gmail.com>
2025-03-22 19:41:02 +0530

    Model Training

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -125,7 +125,7 @@ class ModelTrainer:
 
             r2_square=r2_score(y_test,predicted)
 
-            return r2_square
+            return r2_square,best_model_name
         
         
         except Exception as e:
